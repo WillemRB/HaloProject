82f380ee3805267b31a1c880694d7a8bd381ca2c
WillemRB <wrb@outlook.com>
2015-11-23 13:33:58 +0100

    Add project with Common data.

---
+++ HaloProject.Common/Spartan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HaloProject.Common
+{
+    public class Spartan
+    {
+        #region Information
+        public string GamerTag { get; set; }
+        #endregion
+
+        #region Internal
+        public bool QueuedForUpdate { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+        #endregion
+    }
+}
